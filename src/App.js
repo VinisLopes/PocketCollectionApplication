c6a10f68f1b88da06e13731dbd2dc7af8b66af4f
VinisLopes <viniciusldb2015@gmail.com>
2025-11-02 10:26:10 -0300

    Modificação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// src/App.js - ATUALIZADO COM A ROTA DE EDIÇÃO
-
+// src/App.js - CÓDIGO CORRIGIDO
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'; 
 
 import Login from './pages/Login';
-import Register from './pages/Register';
+import Register from './pages/Register'; // <-- 1. CAMINHO CORRIGIDO
 import PasswordRecovery from './pages/PasswordRecovery';
 import Home from './pages/Home'; 
 import ProtectedRoute from './components/ProtectedRoute'; 
@@ -13,7 +12,9 @@ import ProtectedRoute from './components/ProtectedRoute';
 import MyCollection from './pages/MyCollection';
 import AddItem from './pages/AddItem';
 import ItemDetail from './pages/ItemDetail';
-import EditItem from './pages/EditItem'; // 1. IMPORTAR A NOVA PÁGINA DE EDIÇÃO
+import EditItem from './pages/EditItem';
+import AddWithIA from './pages/AddWithIA';
+import MyShowcase from './pages/MyShowcase'; // Esta linha já estava certa
 
 import './App.css'; 
 
@@ -27,22 +28,20 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/recovery" element={<PasswordRecovery />} />
 
-          {/* Rotas Protegidas (Que usam o layout da Sidebar) */}
+          {/* Rotas Protegidas (Páginas e Modais) */}
           <Route element={<ProtectedRoute />}>
+            {/* Páginas Principais */}
             <Route path="/home" element={<Home />} />
             <Route path="/my-collection" element={<MyCollection />} />
+            <Route path="/add-with-ia" element={<AddWithIA />} />
             
-            {/* Adicione outras rotas do seu sidebar aqui no futuro */}
-            {/* <Route path="/add-with-ia" element={<AddWithIA />} /> */}
-            {/* <Route path="/my-showcase" element={<MyShowcase />} /> */}
-          </Route>
-
-          {/* Rotas de Modal (renderizam por cima de tudo) */}
-          <Route element={<ProtectedRoute />}>
-              <Route path="/add-item" element={<AddItem />} />
-              <Route path="/item/:itemId" element={<ItemDetail />} /> 
-              {/* 2. ADICIONAR A ROTA DE EDIÇÃO */}
-              <Route path="/edit-item/:itemId" element={<EditItem />} />
+            {/* 2. ROTA ADICIONADA (corrige o "never used") */}
+            <Route path="/my-showcase" element={<MyShowcase />} />
+            
+            {/* Rotas de Modal */}
+            <Route path="/add-item" element={<AddItem />} />
+            <Route path="/item/:itemId" element={<ItemDetail />} /> 
+            <Route path="/edit-item/:itemId" element={<EditItem />} />
           </Route>
 
         </Routes>
